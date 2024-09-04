bd44570fe4793d7d17739e08f6f810244b4b3fe0
LuzuraBS <svendsenluzbright@gmail.com>
2024-09-04 08:21:59 +0200

    Luz added tried to add a profile img and some information about the me (Luz)

--- digitalclock/src/ProfileInfo.jsx
+++ digitalclock/src/ProfileInfo.jsx
@@ -1,5 +1,9 @@
 import "./ProfileInfo.css"
-
+const user = {
+        name: `Luz Bright Svendsen`,
+        imageUrl: `https://no.pinterest.com/pin/155092780907854708/`,
+        imageSize: 90, 
+}; 
 
 
 
@@ -15,7 +19,21 @@ return (
 
 
     <>
-   
+        <h1>{user.name}</h1>
+        <img>
+            className="avatar"
+            src={user.imageUrl}
+            alt={`Photo of ` + user.name}
+            style={{
+                width: user.imageSize,
+                height: user.imageSize
+            }}
+           />
+        </>
+      );
+    }
+
+
     </>
 
 
